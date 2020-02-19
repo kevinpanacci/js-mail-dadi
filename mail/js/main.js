e40ab6e67a208e5de64d331cb699776216a0311b
Kevin <kevinpanacci@hotmail.it>
2020-02-19 15:16:04 +0100

    mail fixed -- completato

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,10 +1,11 @@
-var databaseEmail = ['pincopallino@gmail.com', 'carlorossi@gmail.com', 'emailcorretta@gmail.com', 'mariorossi@gmail.com', 'lucarossi@gmail.com'];
+var databaseEmail = ['pincopallino@gmail.com', 'carlorossi@gmail.com', 'random@gmail.com', 'mariorossi@gmail.com', 'lucarossi@gmail.com'];
 var correctEmail = prompt('Inserisci la tua email');
+var x = 'Accesso non consentito'
 
 for (var i = 0; i < databaseEmail.length; i++) {
     if (databaseEmail[i] == correctEmail) {
-        console.log('Accesso consentito');
-    } else {
-        console.log('Accesso non consentito');
+        var x = 'Accesso consentito'
     }
 }
+
+console.log(x);
